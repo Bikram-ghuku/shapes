5705cef95ec24f97c672ceb3d7597b59ffedca8f
Bikram ghuku <bikramghuku05@gmail.com>
2021-06-21 19:04:38 +0530

    added diagonal

--- shapes/__init__.py
+++ shapes/__init__.py
@@ -4,6 +4,7 @@ class rectangle():
     def __init__(self, height, width):
         self.height = height
         self.width = width
+        self.diagonal = (float(height)**2 + float(width)**2)**0.5
 
     def area(self):
         area_op = float(self.height)*float(self.width)
@@ -16,6 +17,7 @@ class rectangle():
 class square():
     def __init__(self, length):
         self.length = length
+        self.diagonal = (2*float(length)**2)**0.5
     
     def area(self):
         area = float(self.length)**2
