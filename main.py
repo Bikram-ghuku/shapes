3c60486146c2db8e30ee036f940e52b1bf2cdfdf
Bikram ghuku <bikramghuku05@gmail.com>
2021-06-21 09:58:43 +0530

    float compatibility+ example

---
+++ main.py
@@ -0,0 +1,13 @@
+import shapes
+
+rectangle = shapes.rectangle(16, 18)
+print("perimeter_rectangle: ",rectangle.perimeter())
+print("area_rectangle: ", rectangle.area())
+
+square = shapes.square(2)
+print("area_square: ",square.area())
+print("perimeter_square: ",square.perimeter())
+
+circle = shapes.circle(7)
+print("circle_perimeter: ", circle.area())
+print("circle_area", circle.perimeter())
--- shapes/__init__.py
+++ shapes/__init__.py
@@ -6,11 +6,11 @@ class rectangle():
         self.width = width
 
     def area(self):
-        area_op = int(self.height)*int(self.width)
+        area_op = float(self.height)*float(self.width)
         return area_op
 
     def perimeter(self):
-        perimeter = 2*(int(self.height) + int(self.width))
+        perimeter = 2*(float(self.height) + float(self.width))
         return perimeter
 
 class square():
@@ -18,11 +18,11 @@ class square():
         self.length = length
     
     def area(self):
-        area = int(self.length)**2
+        area = float(self.length)**2
         return area
 
     def perimeter(self):
-        perimeter = 4*int(self.length)
+        perimeter = 4*float(self.length)
         return perimeter
 
 class circle():
@@ -30,9 +30,9 @@ class circle():
         self.radius = radius
 
     def perimeter(self):
-        perimeter = 2*math.pi*int(self.radius)
+        perimeter = 2*math.pi*float(self.radius)
         return perimeter
 
     def area(self):
-        area = math.pi*int(self.radius)**2
+        area = math.pi*float(self.radius)**2
         return area
