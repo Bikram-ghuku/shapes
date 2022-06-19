00816bfc4e0c464d1678bb8fddf123e8c94d2d26
Bikram Ghuku <37508038+Bikram-ghuku@users.noreply.github.com>
2022-06-19 13:46:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version='0.1.0',
     description='Making calculations of geometric shapes easier',
     author='BikramGhuku',
+    long_description=readme(),
+    url="https://github.com/Bikram-ghuku/shapes",
     license='MIT',
     install_requires=['scipy==1.8.1'],
     tests_require=['pytest==4.4.1'],
