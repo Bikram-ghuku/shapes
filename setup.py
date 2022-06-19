f53ab673660ed36a346719f67011397e5273d2f7
Bikram Ghuku <37508038+Bikram-ghuku@users.noreply.github.com>
2022-06-19 14:00:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 setup(
     name='GeometricShapes',
     packages=find_packages(include=['shapes']),
-    version='0.1.1',
+    version='0.1.2',
     description='Making calculations of geometric shapes easier',
     author='BikramGhuku',
     long_description=readme(),
