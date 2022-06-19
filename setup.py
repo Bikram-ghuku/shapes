3e85614420543870ab556f58a30aad13efc08d90
Bikram Ghuku <37508038+Bikram-ghuku@users.noreply.github.com>
2022-06-19 11:29:41 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+setup(
+    name='GeometricShapes',
+    packages=find_packages(include=['shapes']),
+    version='0.1.0',
+    description='Making calculations of geometric shapes easier',
+    author='BikramGhuku',
+    license='MIT',
+    install_requires=['scipy==1.8.1'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='test.py',
+)
